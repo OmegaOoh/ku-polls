1897e0ec83f25c47de986f0712584f4cf230d4fe
Sirapob PHASUT <omegaooh@gmail.com>
2024-08-21 21:36:07 +0700

    Implement features from Django Tutorial part 2

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    """ Question model to store the question text and its publication date"""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    """ Choices model to store choices of the question with its current number of votes
+        Has Question(class) as a ForeignKey
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
