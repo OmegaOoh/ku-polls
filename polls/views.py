7481066a060c560a7d2058783063eb4f274433b9
Sirapob PHASUT <omegaooh@gmail.com>
2024-08-21 22:11:06 +0700

    Implemented features from Django Tutorial part 3

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
 
-# Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+from polls.models import Question
 
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list,}
+    return render(request, "polls/index.html", context)
+
+def detail(request: HttpRequest, question_id: str) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request: HttpRequest, question_id: str) -> HttpResponse:
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % question_id)
