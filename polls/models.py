9450531f0320b87e413fcd09fbd0e0d6346ed3c6
Sirapob PHASUT <omegaooh@gmail.com>
2024-08-23 23:08:20 +0700

    Implemented features from Django Tutorial part 5

--- polls/models.py
+++ polls/models.py
@@ -13,6 +13,11 @@ class Question(models.Model):
 
     datetime.timedelta(days=1)
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 class Choice(models.Model):
     """ Choices model to store choices of the question with its current number of votes
         Has Question(class) as a ForeignKey
