67fe4fb8f0aa6bf321cca79b52cf744a96b710cb
Sirapob Phasut <omegaooh@gmail.com>
2024-08-30 22:24:18 +0700

    add functionality of end date

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from lib2to3.fixes.fix_input import context
 from typing import Any
 from django.db.models import F
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
@@ -75,12 +76,17 @@ def vote(request: HttpRequest, question_id: int) -> HttpResponse:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
-        return render(request, 'polls/detail.html',
-                  {
-                            "question": question,
-                            "error_message": "You didn't select a choice.",
-                          }
-                     )
+        context = {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        }
+        return render(request, 'polls/detail.html', context=context)
+    if not question.can_vote():
+        context = {
+            "question": question,
+            "error_message": "This polls is closed.",
+        }
+        return render(request, 'polls/detail.html', context=context)
     selected_choice.votes = F("votes") + 1
     selected_choice.save()
     # Redirect user to results page
