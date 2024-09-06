7f86a7f6f11ca60cdcfc8d1f346a793648d4ce1b
Sirapob Phasut <omegaooh@gmail.com>
2024-09-06 23:06:18 +0700

    remove some blank lines

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from typing import Any
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
-from django.db.models import F
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
@@ -34,9 +33,6 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
-
-
-
     def get_queryset(self):
         """
             Excludes any questions that aren't published yet.
@@ -46,11 +42,14 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         """ Override of get_context_data method to add/modify the context data"""
         context = super().get_context_data(**kwargs)
+        # Check for previous selected choice
         if self.request.user.is_authenticated:
             try:
+                # User is authenicated
                 vote = Vote.objects.get(user=self.request.user, choice__question=self.question)
                 context['voted_choice'] = vote.choice.id
             except (Vote.DoesNotExist):
+                # User is not authenicated
                 context['voted_choice'] = None
         return context
 
@@ -97,12 +96,11 @@ class ResultsView(generic.DetailView):
         return super().get(request, *args, **kwargs)
 
 
-
 @login_required
 def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     """ Handle votes POST request from vote button (detail page) """
     question = get_object_or_404(Question, pk=question_id)
-        # Reference to current user
+    # Reference to current user
     cur_user = request.user
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
@@ -124,15 +122,15 @@ def vote(request: HttpRequest, question_id: int) -> HttpResponse:
     try:
         vote = cur_user.vote_set.get(choice__question=question)
         old_choice = vote.choice.choice_text
-        # user has a vote for this question, update the choice.
+        # User has a vote for this question, update the choice.
         vote.choice = selected_choice
         vote.save()
-        messages.success(request,f"Your vote was updated to '{selected_choice.choice_text}'")
+        messages.success(request, f"Your vote was updated to '{selected_choice.choice_text}'")
         logger.info(f"User {cur_user.username} Update vote From {old_choice} to {selected_choice.choice_text} (Question: {question.question_text})") 
     except (KeyError, Vote.DoesNotExist):
-        # user don't have a vote yet
+        # User don't have a vote yet
         vote = Vote.objects.create(user=cur_user, choice=selected_choice)
-        messages.success(request,f"Your voted for '{selected_choice.choice_text}'")
+        messages.success(request, f"Your voted for '{selected_choice.choice_text}'")
         logger.info(f"User {cur_user.username} Vote choice{selected_choice.choice_text} (Question: {question.question_text})")
     selected_choice.save()
     # Redirect user to results page
@@ -146,19 +144,21 @@ def get_client_ip(request):
         return x_forwarded_for.split(',')[0]
     return request.META.get("REMOTE_ADDR")
 
+
 @receiver(user_logged_in)
 def log_user_logged_in(sender, request, user, **kwargs):
     ip_addr = get_client_ip(request)
     logger.info(f"User {user.username} has logged in (IP: {ip_addr})")
 
+
 @receiver(user_logged_out)
 def log_user_logged_out(sender, request, user, **kwargs):
     ip_addr = get_client_ip(request)
-    logger.info (f"User {user.username} has logged out (IP: {ip_addr})")
+    logger.info(f"User {user.username} has logged out (IP: {ip_addr})")
+
 
 @receiver(user_login_failed)
-def log_user_login_failed(sender, credentials, request,**kwargs):
+def log_user_login_failed(sender, credentials, request, **kwargs):
     ip_addr = get_client_ip(request)
     username = credentials['username']
     logger.warn(f"User Failed Login to {username} (IP: {ip_addr})")
-
