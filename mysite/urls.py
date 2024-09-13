5c05ef3b6db9ec7aaa486f8553ef0464d0b9302d
Sirapob Phasut <omegaooh@gmail.com>
2024-09-13 23:13:38 +0700

    remove blank line in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 
-urlpatterns = [
-    
+urlpatterns = [  
     path('polls/', include('polls.urls')),
     path('', RedirectView.as_view(url='/polls/')),
     path('admin/', admin.site.urls),
